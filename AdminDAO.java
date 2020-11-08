49c55af232a1039eb6235b1cd886c2453d44816a
cxl155716 <73486405+cxl155716@users.noreply.github.com>
2020-11-09 00:19:34 +0800

    Add files via upload

---
+++ AdminDAO.java
@@ -0,0 +1,13 @@
+package com.qdu.dao;
+
+import com.qdu.pojo.Administrator;
+/**
+ * AdminDAO接口，定义对Administrator表的操作
+ */
+public interface AdminDAO {
+
+    void update(Administrator admin);
+
+    Administrator getOneById(String id);
+
+}
