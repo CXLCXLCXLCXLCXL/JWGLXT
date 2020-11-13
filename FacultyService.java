181d78fef29be9c3f00ff6a8ee077daed342fe98
ZeroWZZ <74146307+ZeroWZZ@users.noreply.github.com>
2020-11-14 00:17:51 +0800

    Add files via upload

---
+++ FacultyService.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qdu.service;
+
+import com.qdu.dao.FacultyDAO;
+import com.qdu.dao.impl.FacultyDAOImpl;
+import com.qdu.pojo.Faculty;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author master
+ */
+@Service
+public class FacultyService{
+    @Autowired
+    private FacultyDAOImpl dao;
+    
+    @Transactional
+    public Faculty getFacultyByName(String name) {
+        return dao.getOneByName(name);
+    }
+    
+    @Transactional
+    public List getFacultyList() {
+        return dao.getAll();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED,timeout = 10)
+    public void addFaculty(Faculty faculty) {
+         dao.insert(faculty);
+    }
+    
+    @Transactional
+    public void updateFaculty(Faculty faculty) {
+        dao.update(faculty);
+    }
+    
+    @Transactional
+    public void deleteFaculty(String name) {
+        dao.deleteByName(name);
+    }
+    
+    @Transactional
+    public List getListByPageNumberAndSize(int pageNumber, int pageSize) {
+        return dao.getListByPageNumberAndSize(pageNumber, pageSize);
+    }
+
+}
