33bda714c3da722df0cf41dfff58a4fb15d9dbb6
cxl155716 <73486405+cxl155716@users.noreply.github.com>
2020-11-21 23:07:31 +0800

    Update CourseDAO.java

--- CourseDAO.java
+++ CourseDAO.java
@@ -2,17 +2,12 @@ package com.qdu.dao;
 
 import com.qdu.pojo.Course;
 import java.util.List;
-
+/**/
 public interface CourseDAO {
-    //增
     void insert(Course course);
-    //删
     void deleteById(String id);
-    //改
     void update(Course course);
-    //查
     Course getOneById(String id);
-    //查所有
     List<Course> getAll();
     
     List<Course> getListByPageNumberAndSize(int pageNumber, int pageSize);
