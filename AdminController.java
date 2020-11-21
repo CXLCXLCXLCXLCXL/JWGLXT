68b50eee2a8ff8ed73908212ec2d8ca04c73eb8b
ch-ccc-ch <74146952+ch-ccc-ch@users.noreply.github.com>
2020-11-21 23:11:46 +0800

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -32,7 +32,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**/
+/* */
 @Controller
 public class AdminController {
 
