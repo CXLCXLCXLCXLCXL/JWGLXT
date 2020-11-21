6b8da60f988e4ae28a78cc21ff358d0119acc485
ZeroWZZ <74146307+ZeroWZZ@users.noreply.github.com>
2020-11-21 23:46:32 +0800

    Update FacultyService.java

--- FacultyService.java
+++ FacultyService.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.qdu.service;
 
 import com.qdu.dao.FacultyDAO;
@@ -14,10 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- *
- * @author master
- */
+
 @Service
 public class FacultyService{
     @Autowired
