b617e71c59428d13d91efdc8ba005a54d1d9defb
ch-ccc-ch <74146952+ch-ccc-ch@users.noreply.github.com>
2020-11-21 23:11:03 +0800

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -22,16 +22,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+/**/
 @Controller
 public class AdminController {
 
