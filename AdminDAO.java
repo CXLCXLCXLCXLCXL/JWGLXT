d744b5d7e269cb301dca3401cb536b59e8f759a7
ch-ccc-ch <74146952+ch-ccc-ch@users.noreply.github.com>
2020-11-21 23:12:48 +0800

    Update AdminDAO.java

--- AdminDAO.java
+++ AdminDAO.java
@@ -2,8 +2,9 @@ package com.qdu.dao;
 
 import com.qdu.pojo.Administrator;
 /**
- * AdminDAO接口，  定义对Administrator表的操作。
+  AdminDAO接口，  定义对Administrator表的操作。
  */
+
 public interface AdminDAO {
 
     void update(Administrator admin);
