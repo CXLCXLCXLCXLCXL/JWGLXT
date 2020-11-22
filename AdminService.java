0ec426f24daf9f361619980ecb28fe50375a6a96
ch-ccc-ch <74146952+ch-ccc-ch@users.noreply.github.com>
2020-11-22 22:59:24 +0800

    Update AdminService.java

--- AdminService.java
+++ AdminService.java
@@ -5,10 +5,10 @@ import com.qdu.pojo.Administrator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-//事务的使用可以在dao层，也可以在Service层，建议在Service
-//@Transactional:该注解用于指定事务，
-//可以用在类之前，如果用在类前，说明该类中的所有方法使用该事务
-//也可以用在方法前，说明事务使用在该方法中
+/*事务的使用可以在dao层，也可以在Service层，建议在Service
+@Transactional:该注解用于指定事务，
+可以用在类之前，如果用在类前，说明该类中的所有方法使用该事务
+也可以用在方法前，说明事务使用在该方法中*/
 
 @Service
 public class AdminService{
